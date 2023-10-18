24aec9e8aa523594b331f1473fb5581608a1596a
IhateFornitecuziambad <133902909+IhateFortnitecuziambad@users.noreply.github.com>
2023-10-18 07:54:53 +0200

    Create language.js

---
+++ language.js
@@ -0,0 +1,16 @@
+*
+var Lang = navigator.language ;
+if (Lang == "fr-FR") {
+//Setup.html
+document.getElementById("welcome").innerHTML = "Bienvenue";
+document.getElementById("toitweakhub").innerHTML = "Sur iTweakHub";
+document.getElementById("done").innerHTML = "Continuer";
+document.getElementById("termsandprivacy").innerHTML = "Conditions d'utilisation et politique de confidentialité";
+//index.html
+document.getElementById("wb").innerHTML = "Bienvenue,";
+document.getElementById("home").innerHTML = "Accueil";
+document.getElementById("featured").innerHTML = "En vedette";
+document.getElementById("topapps").innerHTML = "Les plus utilisées";
+document.getElementById("newstuto").innerHTML = "Articles et tutoriels,";
+} 
+*/
